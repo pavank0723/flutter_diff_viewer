ce6d17bd442fae9cd39a2eac538c33bee0b79564
Pavan Yadav <pavan.yadav0723@gmail.com>
2026-08-31 17:12:43 +0530

    --ADD horizontal scroll synchronization to `StackedDiffView` via `DiffHorizontalScrollSync`

--- lib/src/presentation/widgets/stacked_diff_view.dart
+++ lib/src/presentation/widgets/stacked_diff_view.dart
@@ -6,6 +6,7 @@ import '../../domain/enums/diff_type.dart';
 import '../builders/diff_builders.dart';
 import '../configuration/diff_viewer_configuration.dart';
 import '../controllers/diff_viewer_controller.dart';
+import '../utils/horizontal_scroll_sync.dart';
 import 'collapsed_section_widget.dart';
 import 'diff_line_widget.dart';
 
@@ -135,7 +136,7 @@ class StackedDiffView extends StatelessWidget {
   }
 }
 
-class _StackedPanel extends StatelessWidget {
+class _StackedPanel extends StatefulWidget {
   final List<_StackedItem> items;
   final bool isOldSide;
   final FlutterDiffViewerConfiguration configuration;
@@ -159,124 +160,150 @@ class _StackedPanel extends StatelessWidget {
   });
 
   @override
+  State<_StackedPanel> createState() => _StackedPanelState();
+}
+
+class _StackedPanelState extends State<_StackedPanel> {
+  late final DiffHorizontalScrollSync _horizontalScrollSync;
+
+  @override
+  void initState() {
+    super.initState();
+    _horizontalScrollSync = DiffHorizontalScrollSync();
+  }
+
+  @override
+  void dispose() {
+    _horizontalScrollSync.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final theme = configuration.theme;
-    final spacing = configuration.spacing;
-    final panelBg = theme.resolvePanelBackgroundColor(isOldSide: isOldSide);
-    final isBlockMode = configuration.splitBlocks || spacing.blockSpacing > 0;
+    final theme = widget.configuration.theme;
+    final spacing = widget.configuration.spacing;
+    final panelBg = theme.resolvePanelBackgroundColor(isOldSide: widget.isOldSide);
+    final isBlockMode = widget.configuration.splitBlocks || spacing.blockSpacing > 0;
+    final blocks = isBlockMode ? _groupStackedBlocks(widget.items) : const <_StackedBlock>[];
 
-    if (isBlockMode) {
-      final blocks = _groupStackedBlocks(items);
-      return Container(
-        color: panelBg,
-        child: ListView.builder(
-          controller: isOldSide
-              ? controller.leftScrollController
-              : controller.rightScrollController,
-          itemCount: blocks.length,
-          itemBuilder: (context, index) {
-            final block = blocks[index];
+    final listWidget = isBlockMode
+        ? ListView.builder(
+            controller: widget.isOldSide
+                ? widget.controller.leftScrollController
+                : widget.controller.rightScrollController,
+            itemCount: blocks.length,
+            itemBuilder: (context, index) {
+              final block = blocks[index];
 
-            if (block.isCollapsed) {
-              final collapsedItem = block.items.first as _CollapsedStackedItem;
-              if (collapsedSectionBuilder != null) {
-                return collapsedSectionBuilder!(
-                  context,
-                  collapsedItem.lineCount,
-                  () => controller.expandSection(collapsedItem.lineIndex),
-                  configuration,
+              if (block.isCollapsed) {
+                final collapsedItem =
+                    block.items.first as _CollapsedStackedItem;
+                if (widget.collapsedSectionBuilder != null) {
+                  return widget.collapsedSectionBuilder!(
+                    context,
+                    collapsedItem.lineCount,
+                    () => widget.controller.expandSection(collapsedItem.lineIndex),
+                    widget.configuration,
+                  );
+                }
+                return CollapsedSectionWidget(
+                  collapsedLineCount: collapsedItem.lineCount,
+                  onExpand: () =>
+                      widget.controller.expandSection(collapsedItem.lineIndex),
+                  configuration: widget.configuration,
                 );
               }
-              return CollapsedSectionWidget(
-                collapsedLineCount: collapsedItem.lineCount,
-                onExpand: () => controller.expandSection(collapsedItem.lineIndex),
-                configuration: configuration,
-              );
-            }
 
-            final blockBg = theme.resolveBlockBackgroundColor(isOldSide: isOldSide);
-            final blockBorder = theme.resolveBlockBorderColor(isOldSide: isOldSide);
+              final blockBg =
+                  theme.resolveBlockBackgroundColor(isOldSide: widget.isOldSide);
+              final blockBorder =
+                  theme.resolveBlockBorderColor(isOldSide: widget.isOldSide);
 
-            return Container(
-              margin: EdgeInsets.only(
-                bottom: index == blocks.length - 1 ? 0 : spacing.blockSpacing,
-              ),
-              padding: spacing.blockPadding,
-              decoration: BoxDecoration(
-                color: blockBg,
-                border: Border.all(
-                  color: blockBorder,
-                  width: spacing.blockBorderWidth,
+              return Container(
+                margin: EdgeInsets.only(
+                  bottom: index == blocks.length - 1 ? 0 : spacing.blockSpacing,
                 ),
-                borderRadius: BorderRadius.circular(spacing.blockBorderRadius),
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: block.items.map((item) {
-                  final lineItem = item as _LineStackedItem;
-                  if (lineBuilder != null) {
-                    return lineBuilder!(context, lineItem.line, configuration);
-                  }
-                  return DiffLineWidget(
-                    key: ValueKey('${isOldSide ? 'old' : 'new'}_${lineItem.lineIndex}'),
-                    line: lineItem.line,
-                    isOldSide: isOldSide,
-                    configuration: configuration,
-                    lineNumberBuilder: lineNumberBuilder,
-                    indicatorBuilder: indicatorBuilder,
-                    segmentBuilder: segmentBuilder,
+                padding: spacing.blockPadding,
+                decoration: BoxDecoration(
+                  color: blockBg,
+                  border: Border.all(
+                    color: blockBorder,
+                    width: spacing.blockBorderWidth,
+                  ),
+                  borderRadius:
+                      BorderRadius.circular(spacing.blockBorderRadius),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: block.items.map((item) {
+                    final lineItem = item as _LineStackedItem;
+                    if (widget.lineBuilder != null) {
+                      return widget.lineBuilder!(
+                          context, lineItem.line, widget.configuration);
+                    }
+                    return DiffLineWidget(
+                      key: ValueKey(
+                          '${widget.isOldSide ? 'old' : 'new'}_${lineItem.lineIndex}'),
+                      line: lineItem.line,
+                      isOldSide: widget.isOldSide,
+                      configuration: widget.configuration,
+                      lineNumberBuilder: widget.lineNumberBuilder,
+                      indicatorBuilder: widget.indicatorBuilder,
+                      segmentBuilder: widget.segmentBuilder,
+                      horizontalScrollSync: _horizontalScrollSync,
+                    );
+                  }).toList(growable: false),
+                ),
+              );
+            },
+          )
+        : ListView.builder(
+            controller: widget.isOldSide
+                ? widget.controller.leftScrollController
+                : widget.controller.rightScrollController,
+            itemCount: widget.items.length,
+            itemExtent: widget.configuration.spacing.lineHeight,
+            itemBuilder: (context, index) {
+              final item = widget.items[index];
+
+              if (item is _CollapsedStackedItem) {
+                if (widget.collapsedSectionBuilder != null) {
+                  return widget.collapsedSectionBuilder!(
+                    context,
+                    item.lineCount,
+                    () => widget.controller.expandSection(item.lineIndex),
+                    widget.configuration,
                   );
-                }).toList(growable: false),
-              ),
-            );
-          },
-        ),
-      );
-    }
+                }
+                return CollapsedSectionWidget(
+                  collapsedLineCount: item.lineCount,
+                  onExpand: () => widget.controller.expandSection(item.lineIndex),
+                  configuration: widget.configuration,
+                );
+              }
+
+              final lineItem = item as _LineStackedItem;
+              if (widget.lineBuilder != null) {
+                return widget.lineBuilder!(context, lineItem.line, widget.configuration);
+              }
+              return DiffLineWidget(
+                key: ValueKey(
+                    '${widget.isOldSide ? 'old' : 'new'}_${lineItem.lineIndex}'),
+                line: lineItem.line,
+                isOldSide: widget.isOldSide,
+                configuration: widget.configuration,
+                lineNumberBuilder: widget.lineNumberBuilder,
+                indicatorBuilder: widget.indicatorBuilder,
+                segmentBuilder: widget.segmentBuilder,
+                horizontalScrollSync: _horizontalScrollSync,
+              );
+            },
+          );
 
     return Container(
       color: panelBg,
-      child: ListView.builder(
-        controller: isOldSide
-            ? controller.leftScrollController
-            : controller.rightScrollController,
-        itemCount: items.length,
-        itemExtent: configuration.spacing.lineHeight,
-        itemBuilder: (context, index) {
-          final item = items[index];
-
-          if (item is _CollapsedStackedItem) {
-            if (collapsedSectionBuilder != null) {
-              return collapsedSectionBuilder!(
-                context,
-                item.lineCount,
-                () => controller.expandSection(item.lineIndex),
-                configuration,
-              );
-            }
-            return CollapsedSectionWidget(
-              collapsedLineCount: item.lineCount,
-              onExpand: () => controller.expandSection(item.lineIndex),
-              configuration: configuration,
-            );
-          }
-
-          final lineItem = item as _LineStackedItem;
-          if (lineBuilder != null) {
-            return lineBuilder!(context, lineItem.line, configuration);
-          }
-          return DiffLineWidget(
-            key: ValueKey('${isOldSide ? 'old' : 'new'}_${lineItem.lineIndex}'),
-            line: lineItem.line,
-            isOldSide: isOldSide,
-            configuration: configuration,
-            lineNumberBuilder: lineNumberBuilder,
-            indicatorBuilder: indicatorBuilder,
-            segmentBuilder: segmentBuilder,
-          );
-        },
-      ),
+      child: listWidget,
     );
   }
 }
